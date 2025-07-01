a7301e86d9802110ba921d9571717a38c98d617b
Jujock004 <julien.joecker@gmail.com>
2025-07-01 15:32:23 +0200

    Revert "feat: add a contact form"
    This reverts commit 43632517c58724775a999f59cc3d7ff29aaf7ef0.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import './App.css'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
-import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <div className="bg-background text-foreground">
-      <Toaster />
       <Header />
       <Hero />
       <Footer />
