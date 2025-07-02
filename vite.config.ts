4d69d197b1b898a7bca832d655dc5a2a8d63d4bd
Julien Joecker <julien.joecker@gmail.com>
2025-07-03 00:10:45 +0200

    fix: correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Jujock004.github.io/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
