68321ca2a8b65b29f61269824d0d35db7df4cb56
Julien Joecker <julien.joecker@gmail.com>
2025-07-03 00:30:48 +0200

    fix: correct import images

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,26 +1,34 @@
+import jestoneImg from '@/assets/images/jestone.png';
+import pokemonImg from '@/assets/images/pokemon-arena.png';
+import oheImg from '@/assets/images/ohe.png';
+import wildlyDoneImg from '@/assets/images/wildly-done.png';
+
 export const projectList = [
-    {
-        name: 'JESTONE',
-        description: 'Developing a freemium OTT video hosting platform for a fictive company.',
-        image: 'src/assets/images/jestone.png',
-        link: 'https://github.com/Jujock004/jestone',
-    },
-    {
-        name: 'Pokemon Arena',
-        description: 'Development of an interactive Pokédex and tournament system.',
-        image: 'src/assets/images/pokemon-arena.png',
-        link: 'https://github.com/Jujock004/pokemon-arena',
-    },
-    {
-        name: 'Ohé',
-        description: 'Community events platform with secure authentication and event management.',
-        image: 'src/assets/images/ohe.png',
-        link: 'https://github.com/Jujock004/ohe',
-    },
-    {
-        name: 'Wildly Done',
-        description: 'Development of a task management application in a 4-person team.',
-        image: 'src/assets/images/wildly-done.png',
-        link: 'https://github.com/Jujock004/Project-1-TD3.github.io',
-    },
+  {
+    name: 'JESTONE',
+    description:
+      'Developing a freemium OTT video hosting platform for a fictive company.',
+    image: jestoneImg,
+    link: 'https://github.com/Jujock004/jestone',
+  },
+  {
+    name: 'Pokemon Arena',
+    description: 'Development of an interactive Pokédex and tournament system.',
+    image: pokemonImg,
+    link: 'https://github.com/Jujock004/pokemon-arena',
+  },
+  {
+    name: 'Ohé',
+    description:
+      'Community events platform with secure authentication and event management.',
+    image: oheImg,
+    link: 'https://github.com/Jujock004/ohe',
+  },
+  {
+    name: 'Wildly Done',
+    description:
+      'Development of a task management application in a 4-person team.',
+    image: wildlyDoneImg,
+    link: 'https://github.com/Jujock004/Project-1-TD3.github.io',
+  },
 ];
